35ac9c8081836d35ced36c0f73ffd32c50f8f1ac
Alesh Houdek <mail@alesh.com>
2018-02-18 07:08:31 -0700

    configured sass preprocessor, moved css from template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,77 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cannonball</title>
 
-  <style>
-    /* @import url('https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i'); */
-    @import url('https://fonts.googleapis.com/css?family=Tillana:400,600,800');
-
-
-    * {
-      margin: 0;
-      padding: 0;
-      font-family: 'Tillana', cursive;
-    }
-
-    *, *:before, *:after {
-      box-sizing: border-box;
-    }
-
-    a {
-      text-decoration: none;
-      color: ;
-    }
-    strong {
-      font-weight: 600;
-    }
-    body {
-      background-color: #999;
-      color: 333;
-    }
-    header, main {
-      padding: 10px 25px;
-    }
-    header {
-      position: fixed;
-      top: 0;
-      width: 100%;
-      border-bottom: 1px solid #999;
-      background-color: #eee;
-      opacity: .98;
-    }
-    main {
-      margin-top: 68px;
-      background-color: #eee;
-    }
-    nav {
-      display: inline;
-    }
-    nav a {
-      margin-right: 10px;
-    }
-    header h1 {
-      display: inline;
-      font-weight: 800;
-      font-size: 25px;
-      margin-right: 10px;
-    }
-    h2 {
-      font-weight: 600;
-      font-size: 35px;
-      margin-bottom: 5px;
-    }
-    ul {
-      margin-left: 20px;
-    }
-    .cragle {
-      z-index: -1;
-      position: absolute;
-      background-color: #eee;
-      top: 0;
-      width: 100%;
-      height: 100%;
-    }
-  </style>
-
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
   <div class="cragle"></div>
