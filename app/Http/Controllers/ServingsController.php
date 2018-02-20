1c23526e48c4f8af7e5f5e2462f408dd201ccb41
Alesh Houdek <mail@alesh.com>
2018-02-20 06:32:21 -0700

    servings view shows today and older

--- app/Http/Controllers/ServingsController.php
+++ app/Http/Controllers/ServingsController.php
@@ -4,14 +4,17 @@ namespace Cannonball\Http\Controllers;
 
 use Cannonball\Serving;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ServingsController extends Controller
 {
 
     public function index()
     {
-        $servings = Serving::latest()->get();
-        return view('servings.index', compact('servings'));
+        $servings = Serving::where('created_at', '>=', Carbon::today())->latest()->get();
+        $oldServings = Serving::where('created_at', '<', Carbon::today())->latest()->get();
+
+        return view('servings.index', compact('servings', 'oldServings'));
     }
 
     public function store(Request $request)
--- resources/views/servings/index.blade.php
+++ resources/views/servings/index.blade.php
@@ -3,19 +3,38 @@
 
 @section('content')
 
-  <h2>Servings</h2>
+  <h2>Today</h2>
 
   @foreach ($servings as $serving)
 
     <div class="show-serving">
-      Serving:<br>
-      {{ $serving->beverage->name }}
-      <br>
       {{ $serving->created_at->diffForHumans() }}
+      <br>
+      <h3>
+        {{ $serving->beverage->name }}
+      </h3>
     </div>
     <br>
 
   @endforeach
 
+
+  @if($oldServings->count())
+
+    <h2>Older</h2>
+
+    @foreach ($oldServings as $serving)
+
+      <div class="show-serving">
+        {{ $serving->created_at->diffForHumans() }}
+        <br>
+        <h3>
+          {{ $serving->beverage->name }}
+        </h3>
+      </div>
+      <br>
+    @endforeach
+
+  @endif
 
 @endsection
